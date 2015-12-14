7abdf28f1967efd9de5f179de3c698c66f9b008f
Mike Qin <mikeandmore@gmail.com>
2015-12-14 06:13:26 +0000

    switch to CMake

---
+++ gen-iso.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+KERN_IMG=$1
+SOURCE_DIR=`dirname $0`
+
+pushd $SOURCE_DIR
+
+cp thirdparty/grub/grub.cfg isofiles/boot/grub/
+cp thirdparty/grub/grub.img isofiles/boot/grub/
+cp ${KERN_IMG} isofiles/boot/
+touch isofiles/boot/grub/stage2
+xorriso -as mkisofs -R -b boot/grub/grub.img -no-emul-boot -boot-load-size 4 -boot-info-table -o boot.iso isofiles
+
+popd
